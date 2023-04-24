95b336681de6c7d55c954b168c1fd7720bdc2eb4
Likhithanwmsu <123027051+Likhithanwmsu@users.noreply.github.com>
2023-04-24 15:43:10 -0500

    commited updated files

--- models/hotels.js
+++ models/hotels.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose")
 const hotelsSchema = mongoose.Schema({
-hotels_location: String,
-hotels_numrooms: Number,
-hotels_rating: Number
-})
+// hotels_location: String,
+// hotels_numrooms: Number,
+// hotels_rating: Number
+
+//New code for Assignment13
+hotels_location: {
+    type: String,
+    required: true
+},
+hotels_numrooms: {
+    type: Number,
+    required: true,
+    min:0,
+    max:100
+},
+hotels_rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 10
+}
+});
+
 module.exports = mongoose.model("hotels",
 hotelsSchema)
--- routes/hotels.js
+++ routes/hotels.js
@@ -6,14 +6,26 @@ const hotels_controlers = require('../controllers/hotels');
 /* GET costumes */
 
 router.get('/', hotels_controlers.hotels_view_all_Page);
+router.get('/hotels/:id', hotels_controlers.hotels_detail);
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 
 module.exports = router;
 /* GET detail costume page */
-router.get('/detail', hotels_controlers.hotels_view_one_Page);
+router.get('/detail',secured, hotels_controlers.hotels_view_one_Page);
 /* GET create costume page */
-router.get('/create', hotels_controlers.hotels_create_Page);
+router.get('/create',secured, hotels_controlers.hotels_create_Page);
 /* GET create update page */
-router.get('/update', hotels_controlers.hotels_update_Page);
+router.get('/update',secured, hotels_controlers.hotels_update_Page);
 /* GET delete costume page */
-router.get('/delete', hotels_controlers.hotels_delete_Page);
+router.get('/delete',secured, hotels_controlers.hotels_delete_Page);
 
